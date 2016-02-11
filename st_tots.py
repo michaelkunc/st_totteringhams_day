fac4dea8789cf7a68f2d299a7ddb9eea6c4187b2
Michael Kunc <mjkunc@gmail.com>
2016-02-11 08:53:37 -0600

    several methods and start of test suite

--- st_tots.py
+++ st_tots.py
@@ -1,14 +1,21 @@
 
-#just kinda mocking in some methods I think I will need
+GAMES_IN_SEASON = 38
+POINTS_PER_WIN = 3
 
 #will retrieve current table data for Arsenal and Sp*rs
 def get_table():
-	print 'is this thing on?'
-
+	return 'is this thing on?'
 
 #compare current point to available points
 def compare_difference_available(point_difference, available_points):
 	return point_difference > available_points
 
+#return the difference in pts between arsenal and spurs
+def points_difference(arsenal_pts, spurs_pts):
+	return arsenal_pts - spurs_pts
 
-print compare_difference_available(9, 6)
+#return the remaining available points for a given number of games played
+def get_available_points(games_played):
+	return (GAMES_IN_SEASON - games_played) * POINTS_PER_WIN
+
+
---
+++ st_tots_test.py
@@ -0,0 +1,26 @@
+import unittest
+import st_tots
+
+class ST_TOTSTESTS(unittest.TestCase):
+
+	def test_get_table(self):
+		self.assertEqual('is this thing on?', st_tots.get_table())
+
+	def test_compare_difference_available(self):
+		point_difference = 6
+		available_pts = 30
+		result = st_tots.compare_difference_available(point_difference, available_pts)
+		self.assertFalse(result)
+
+	def test_points_difference(self):
+		arsenal_pts = 58
+		spurs_pts = 55
+		self.assertEqual(3, st_tots.points_difference(arsenal_pts, spurs_pts))
+
+	def test_get_available_points(self):
+		games_played = 25
+		self.assertEqual(39, st_tots.get_available_points(games_played))
+
+
+if __name__ == '__main__':
+	unittest.main()
