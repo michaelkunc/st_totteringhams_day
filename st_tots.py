7e4ebf241f6a47b28d588107a8b1e0621662824f
Michael Kunc <mjkunc@gmail.com>
2016-02-17 17:24:26 -0600

    get_team method now working

--- st_tots.py
+++ st_tots.py
@@ -4,11 +4,6 @@ from urllib2 import urlopen
 TABLE_URL = "http://www.premierleague.com/en-gb/matchday/league-table.html"
 GAMES_IN_SEASON = 38
 POINTS_PER_WIN = 3
-arsenal_pts = 0
-spurs_pts = 0
-arsenal_games_remaining = 0
-spurs_games_remaining = 0
-
 
 # will retrieve current table data for Arsenal and Sp*rs
 def get_table():
@@ -31,13 +26,8 @@ def make_soup(url):
     html = urlopen(url).read()
     return BeautifulSoup(html, "lxml")
 
-#this isn't working exactly right, so I'm going to build
-#the method line by line
-#I'm entering this in ipython line by line and it's working 
-#as expected there. but this method is returning an empty array.
-#a bit of a loss as to why that is.
 def get_team(url, tag, team):
     html = urlopen(url).read()
     soup = BeautifulSoup(html, 'lxml')
-    club = soup.find_all(tag, text='team')
-    return club
+    return soup.find_all(tag, text=team)
+    
