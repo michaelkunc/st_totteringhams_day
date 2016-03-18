06fbc5e6b9bae1e16f6a1bc77dc99f05e2620c39
michaelkunc <mjkunc@gmail.com>
2016-03-18 06:31:40 -0500

    rename some test setup variables

--- st_tots_test.py
+++ st_tots_test.py
@@ -7,13 +7,13 @@ class ST_TOTSTESTS(unittest.TestCase):
 		ST_TOTSTESTS.points_per_win = 3
 		ST_TOTSTESTS.games_per_season = 38
 		ST_TOTSTESTS.max_points = ST_TOTSTESTS.games_per_season * ST_TOTSTESTS.points_per_win
-		ST_TOTSTESTS.soup = st_tots.Soup('http://www.premierleague.com/en-gb/matchday/league-table.html')
-		ST_TOTSTESTS.arsenal = st_tots.Team('Arsenal', ST_TOTSTESTS.soup)
-		ST_TOTSTESTS.spurs = st_tots.Team('Tottenham Hotspur', ST_TOTSTESTS.soup)
+		ST_TOTSTESTS.table_soup = st_tots.Soup('http://www.premierleague.com/en-gb/matchday/league-table.html')
+		ST_TOTSTESTS.arsenal = st_tots.Team('Arsenal', ST_TOTSTESTS.table_soup)
+		ST_TOTSTESTS.spurs = st_tots.Team('Tottenham Hotspur', ST_TOTSTESTS.table_soup)
 
 #need to review this test to make sure it's testing something valuable.
 	def test_init_Soup(self):
-		class_name = str(type(ST_TOTSTESTS.soup))
+		class_name = str(type(ST_TOTSTESTS.table_soup))
 		self.assertEqual("<class 'st_tots.Soup'>", class_name)
 
 	def test_Team__init__games_played(self):
@@ -25,7 +25,7 @@ class ST_TOTSTESTS(unittest.TestCase):
 
 #this is a slightly gnarley test case.
 	def test_Team_get_team_data(self):
-		self.assertEqual('<td class="col-pos">3</td>', str(ST_TOTSTESTS.arsenal.get_team_data(ST_TOTSTESTS.soup, 'td', "Arsenal")[0]))
+		self.assertEqual('<td class="col-pos">3</td>', str(ST_TOTSTESTS.arsenal.get_team_data(ST_TOTSTESTS.table_soup, 'td', "Arsenal")[0]))
 
 
 	def test_Team_available_points(self):
@@ -36,9 +36,9 @@ class ST_TOTSTESTS(unittest.TestCase):
 
 	def test_check_for_st_tots_achieved(self):
 		#stubbing out attributes to trigger the else condition
-		arsenal = st_tots.Team('Arsenal', ST_TOTSTESTS.soup)
+		arsenal = st_tots.Team('Arsenal', ST_TOTSTESTS.table_soup)
 		arsenal.points = 100
-		spurs = st_tots.Team('Tottenham Hotspur', ST_TOTSTESTS.soup)
+		spurs = st_tots.Team('Tottenham Hotspur', ST_TOTSTESTS.table_soup)
 		spurs.points = 0
 		self.assertTrue(st_tots.st_tots(arsenal, spurs))
 
@@ -52,10 +52,10 @@ class ST_TOTSTESTS(unittest.TestCase):
 
 	def test_simulate_remaining_season_end_of_season(self):
 		#stubbing attributes to trigger the elif condition
-		arsenal = st_tots.Team('Arsenal', ST_TOTSTESTS.soup)
+		arsenal = st_tots.Team('Arsenal', ST_TOTSTESTS.table_soup)
 		arsenal.games_played = 38
 		arsenal.points = 0
-		spurs = st_tots.Team('Tottenham Hotspur', ST_TOTSTESTS.soup)
+		spurs = st_tots.Team('Tottenham Hotspur', ST_TOTSTESTS.table_soup)
 		spurs.games_played = 38
 		spurs.points = 0
 		self.assertEqual( st_tots.Messages.end_of_season_message(), st_tots.simulate_remaining_season(arsenal, spurs))
